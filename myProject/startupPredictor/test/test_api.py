97902cae5beaa8201d7850d9a19590ad12e07013
Siya Sharma <siyaduttsharma@gmail.com>
2023-03-04 18:09:49 -0500

    frontend: removed devenv and myProject
    Cleaning my branch.

--- myProject/startupPredictor/test/test_api.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-class ApiTests(TestCase):
-    def setUp(self):
-        pass
-
-    def test_api_exists(self):
-        """Animals that can speak are correctly identified"""
-        self.assertEqual(False, True)
--- myProject/startupPredictor/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import my_view, predict
-
-urlpatterns = [
-    path('', my_view, name='home'),
-    path('api/', predict, name='model')
-]
