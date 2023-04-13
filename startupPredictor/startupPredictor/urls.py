22a5de990187beab158fed7231e01b72c92210b5
Siya Sharma <siyaduttsharma@gmail.com>
2023-04-13 00:57:54 -0400

    frontend: latest frontend

--- startupPredictor/startupPredictor/urls.py
+++ startupPredictor/startupPredictor/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('roi/', my_view, name='roi'),
     path('api/', predict, name='model'),
     path('home/', homepage, name='home'),
+    path('', homepage, name='home'),
+    path('results/', results_view, name='results_view'),
 ]
--- startupPredictor/startupPredictor/views.py
+++ startupPredictor/startupPredictor/views.py
@@ -1,17 +1,33 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from ml2 import roipred
 
 def my_view(request):
     if request.method == 'POST':
         # process the user's input
         startup_name = request.POST.get('startup-name')  # extract the 'startup-name' field
-        # perform some operation using the startup_name input
-        result = startup_name
-        # return a response
-        return render(request, 'startup_details.html', {'result': result})
-    else:
-        # display the input form
-        return render(request, 'input_form.html')
+
+        # store the startup name in a session variable
+        request.session['startup_name'] = startup_name
+
+        # show the progress page
+        return render(request, 'progress.html')
+
+    return render(request, 'input_form.html')
+
+
+def results_view(request):
+    # Get the startup name from the session variable
+    startup_name = request.session.get('startup_name')
+    
+    # Perform some operation to get the result
+    result = startup_name
+    
+    # pass ROI to results page
+    context = {'result': result}
+        
+    # return the results page
+    return render(request, 'startup_details.html', context)
 
     
 def predict(request):
