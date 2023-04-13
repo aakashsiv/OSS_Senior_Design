696ebd7cec3312872469108af1e3d19305854c12
siyaduttsharma <67598486+siyaduttsharma@users.noreply.github.com>
2023-04-13 02:42:42 -0400

    Update getCSV.py
     Download the CSV file from the URL and save it to a local file

--- utilities/getCSV.py
+++ utilities/getCSV.py
@@ -1,11 +1,8 @@
-#this gets a csv file from the github repo and downloads it locally
-
+# Download the CSV file from the URL and save it to a local file
 import requests
 
-# URL of the CSV file
 url = 'https://raw.githubusercontent.com/aakashsiv/OSS_Senior_Design/feature/API_data/20230329000001952/10.csv'
-
-# Download the CSV file from the URL and save it locally
 res = requests.get(url, allow_redirects=True)
-with open('csv10.csv', 'wb') as file:
+
+with open('myCSV.csv', 'wb') as file:
     file.write(res.content)
